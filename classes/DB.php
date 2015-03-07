7b77265dd8a89e8d385333658f46bfa5a8ab6346
wzym <wzym@yandex.ru>
2015-03-07 17:36:12 +0300

    403-ошибка выбрасывается как при неудачном подключении, так и при неудачном запросе.

--- classes/DB.php
+++ classes/DB.php
@@ -5,7 +5,15 @@ class DB {
     private $className = 'stdClass';
 
     public function __construct() {
-        $this->dbh = new PDO('mysql:dbname=lesson01;host=localhost', 'root', '');
+        try{
+            $this->dbh = new PDO('mysql:dbname=lesson010;host=localhost', 'root', '');
+        } catch (PDOException $e) {
+            $logText = var_dump($e);
+            echo $logText;
+            $exc403 = new E403Exception('Соединиться с БД не удалось. В остальном всё работает как часы.');
+            throw $exc403;
+        }
+
     }
 
     public function setClassName($className) {
@@ -14,7 +22,13 @@ class DB {
 
     public function query($sql, $params=[], $isReturnable) {
         $sth = $this->dbh->prepare($sql);
-        $res = $sth->execute($params);
+
+        try {
+            $res = $sth->execute($params);
+        } catch (PDOException $errPDO) {
+            $exc403 = new E403Exception('Удалось даже соединиться, но запрос оказался некорректным.');
+            throw $exc403;
+        }
         if (true === $isReturnable) {
             return $sth->fetchAll(PDO::FETCH_CLASS, $this->className);
         }
