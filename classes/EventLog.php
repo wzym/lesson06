1135bf4ed451927abb4926fd9e0fb4113db55d4b
wzym <wzym@yandex.ru>
2015-03-08 20:19:21 +0300

    Рабочая версия  функции записи в лог.

--- classes/EventLog.php
+++ classes/EventLog.php
@@ -15,6 +15,15 @@ class EventLog {
     }
 
     private function saveEvent() {
-
+        $handle = fopen(__DIR__ . '/../views/log.html', 'a');
+        $stringToAdd = '<div>' .
+                        'Время события: ' . $this->time . '.<br />' .
+                        'Сообщение: ' . $this->message . '.<br />' .
+                        'Файл с ошибкой скрипта: ' . $this->filename . '.<br />' .
+                        'Строка ошибки:' . $this->lineNum . '.<br /><br />' .
+                        '</div>
+                        ';
+        fwrite($handle, $stringToAdd);
+        fclose($handle);
     }
 }
